67558062377bbba94b1b5d8da4cbb705fd424525
Alexey <balabolinad@gmail.com>
2022-06-07 10:20:51 +0300

    Teleport player

--- ECS_Test/Assets/Scripts/Components/MoveDataComponent.cs
+++ ECS_Test/Assets/Scripts/Components/MoveDataComponent.cs
@@ -1,13 +1,16 @@
-﻿using Unity.Entities;
+﻿using System;
+using System.Collections;
+using Unity.Entities;
 using UnityEngine;
 
 public class MoveDataComponent:MonoBehaviour, IConvertGameObjectToEntity
 {
     [SerializeField] float _speed;
     [SerializeField] float _teleportDistance;
+    [SerializeField] float _teleportRechargeTime;
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
-        dstManager.AddComponentData(entity, new MoveData(_speed, _teleportDistance));
+        dstManager.AddComponentData(entity, new MoveData(_speed, _teleportDistance, _teleportRechargeTime));
     }
 }
 
@@ -15,9 +18,12 @@ public struct MoveData:IComponentData
 {
     public float Speed { get; set; }
     public float TeleportDistance { get; set; }
-    public MoveData(float speed, float teleportDistance)
+
+    public float TeleportRechargeTime { get; set; }
+    public MoveData(float speed, float teleportDistance, float teleportRechargeTime)
     {
         Speed = speed;
         TeleportDistance = teleportDistance;
+        TeleportRechargeTime = teleportRechargeTime;
     }
 }
--- ECS_Test/Assets/Scripts/Systems/PlayerMoveSystem.cs
+++ ECS_Test/Assets/Scripts/Systems/PlayerMoveSystem.cs
@@ -1,10 +1,14 @@
-﻿using Unity.Entities;
+﻿using System.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerMoveSystem : ComponentSystem
 {
     //query for Entity selection
     private EntityQuery _moveQuery;
+    private bool _isTeleport = true;
+    private float _teleportTimer = 0;
 
     protected override void OnCreate()
     {
@@ -16,9 +20,11 @@ public class PlayerMoveSystem : ComponentSystem
 
     protected override void OnUpdate()
     {
-        Entities.With(_moveQuery).ForEach((Entity entity, Transform transform, ref InputData inputData, ref MoveData moveData) =>
+        Entities.With(_moveQuery).ForEach((Entity entity, Transform transform,
+            ref InputData inputData, ref MoveData moveData) =>
         {
             MovePlayer(transform, inputData, moveData);
+            TeleportPlayer(transform, inputData, moveData);
         });
     }
 
@@ -26,8 +32,37 @@ public class PlayerMoveSystem : ComponentSystem
     {
         var position = transform.position;
         position += new Vector3(inputData.MoveVector.x, 0, inputData.MoveVector.y) * Time.DeltaTime * moveData.Speed;
-        if (inputData.IsTeleport)
-            position += transform.forward * moveData.TeleportDistance;
         transform.position = position;
+        if (inputData.MoveVector.x!=0 && inputData.MoveVector.y!=0)
+            transform.forward = new Vector3(inputData.MoveVector.x, 0, inputData.MoveVector.y);
+    }
+
+    private void TeleportPlayer(Transform transform, InputData inputData, MoveData moveData)
+    {
+        if(inputData.IsTeleport)
+        {
+            if (_isTeleport)
+            {
+                var position = transform.position;
+                position += transform.forward * moveData.TeleportDistance;
+                transform.position = position;
+                _isTeleport = false;               
+            }
+        }
+        if(!_isTeleport)
+        {
+            _teleportTimer += Time.DeltaTime;
+            if (_teleportTimer >= moveData.TeleportRechargeTime)
+            {
+                _isTeleport = true;
+                _teleportTimer = 0;
+            }
+        }
+    }
+
+    private IEnumerator RechargeTeleport(float time)
+    {
+        yield return new WaitForSeconds(time);
+        _isTeleport = true;
     }
 }
